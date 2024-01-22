0109ecd33f25c0b33561351ca68bb93f71f63039
window1 <doserack@gmail.com>
2024-01-22 09:53:13 +0900

    Initial Commit

---
+++ SideMenu/SideMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenuApp.swift
+//  SideMenu
+//
+//  Created by window1 on 1/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SideMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
